3c0d5d12b4148be537e42ad3cea28d022085d4f4
Ozzy Lozano <oziellozanouwu@gmail.com>
2022-09-08 08:21:18 -0500

    updATE

--- recursividad/ejercicio05.java
+++ recursividad/ejercicio05.java
@@ -8,14 +8,26 @@ public class ejercicio05 {
   }
 
   void fibonacci() {
-    if (num3 > 1000) System.out.println("fin c:");
-    else {
-      num3 = num1 + num2;
-      num1 = num2;
-      num2 = num3;
-      System.out.println(i + "." + num3);
+    if (i == 1) {
+      System.out.println(i + "." + num1);
       i++;
       fibonacci();
+    }
+    else if (i == 2) {
+      System.out.println(i + "." + num2);
+      i++;
+      fibonacci();
+    }
+    else{
+      if (num3 > 1000) System.out.println("fin c:");
+      else {
+        num3 = num1 + num2;
+        num1 = num2;
+        num2 = num3;
+        System.out.println(i + "." + num3);
+        i++;
+        fibonacci();
+      }
     }
   }
 }
