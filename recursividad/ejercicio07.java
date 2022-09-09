e7c119582781d52b2f113cac4d4536cf09c7b367
Ozzy Lozano <oziellozanouwu@gmail.com>
2022-09-09 14:49:53 -0500

    update

--- recursividad/ejercicio07.java
+++ recursividad/ejercicio07.java
@@ -3,19 +3,23 @@ package recursividad;
 import java.util.Scanner;
 
 public class ejercicio07 {
-  int inverso;
+  long inverso;
   public static void main(String[] args) {
     ejercicio07 ex = new ejercicio07();
     Scanner read = new Scanner(System.in);
     System.out.println("ingresa un numerito c:");
-    int n = read.nextInt();
+    long n = read.nextLong();
     ex.num_inverso(n);
+    System.out.println(ex.inverso);
     read.close();
   }
 
-  void num_inverso(int n) {
-    System.out.print(n%10);
-    if(n/10 != 0) num_inverso(n / 10);
-    else System.out.println();
+  void num_inverso(long n) {
+    inverso += n%10;
+    if(n/10 != 0) {
+      inverso*= 10;
+      num_inverso(n / 10);
+    }
+    else System.out.print("tu numero al reves es: ");
   }
 }
