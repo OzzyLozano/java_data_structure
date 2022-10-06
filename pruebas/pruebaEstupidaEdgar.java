85b234a5bae825bfb32da7627c3d82168d179e5b
Ozzy Lozano <oziellozanouwu@gmail.com>
2022-10-06 10:23:52 -0500

    documented Edgars test

--- pruebas/pruebaEstupidaEdgar.java
+++ pruebas/pruebaEstupidaEdgar.java
@@ -3,6 +3,8 @@ package pruebas;
 import java.util.Scanner;
 
 public class pruebaEstupidaEdgar {
+  // i here declare my variables, the arrays are so that i can check easily wich
+  // month numbers have 31 or 30, their for to be iterated
   static int dias31[] = {1, 3, 5, 7, 8, 10, 12}, dias30[] = {4, 6, 9, 11};
   static int dia, mes, year;
 
@@ -14,11 +16,13 @@ public class pruebaEstupidaEdgar {
     mes = read.nextInt();
     System.out.print("ingrese year: ");
     year = read.nextInt();
-    read.close();
-    verificar(dia, mes);
+    read.close(); // closing my scanner variable, for i wont use it anymore, and consumes memory
+    verificar(dia, mes); // here we pass day and month to check if theyre to be a real date
     System.out.println("tu fecha es: " + dia + "/" + mes + "/" + year);
   }
 
+  // after going to the check function it should return true or false, and this function
+  // has the conditions as to what to do in wich case of month
   static void verificar(int dia, int mes) {
     if(check(mes, dias31)) {
       if (!(dia < 32) || !(dia > 0)) noTiene(mes, dia);
@@ -36,8 +40,11 @@ public class pruebaEstupidaEdgar {
     System.out.println("el mes " + mes + " no tiene " + dia + " dias");
   }
 
+  // the check function here cheks if the arrays we declared at the start of our class
+  // contains the month the user entered, i used this so we have a cleaner code
   static boolean check(int mes, int dias[]) {
     for (int i : dias) if (i == mes) return true;
     return false;
-  }
+  } // once we know that our month is or isnt in the selected, 
+  // the 'verificar' function will take care of the rest
 }
